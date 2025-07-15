2a0a29acfa044a9fe73fb905a95bd6a65e84a08d
niranjanshk <niranjanshk27@gmail.com>
2025-07-15 22:20:46 +0545

    Update key map

--- lua/niranjan/lazy/treesitter.lua
+++ lua/niranjan/lazy/treesitter.lua
@@ -21,6 +21,17 @@ return {
                     enable = true
                 },
 
+                fold = {
+                    enable = true,
+                --     disable = function(lang, buf)
+                --         local max_filesize = 100 * 1024 -- 100 KB
+                --         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                --         if ok and stats and stats.size > max_filesize then
+                --             return true
+                --         end
+                --     end,
+                },
+
                 highlight = {
                     -- `false` will disable the whole extension
                     enable = true,
