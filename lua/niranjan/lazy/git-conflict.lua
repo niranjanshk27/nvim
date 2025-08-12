cd7f35a89b4c78eca186cc2419cb45720906b8f5
niranjanshk <niranjanshk27@gmail.com>
2025-08-12 16:07:06 +0545

    fix git-conflict

--- lua/niranjan/lazy/git-conflict.lua
+++ lua/niranjan/lazy/git-conflict.lua
@@ -1,18 +1,41 @@
 return {
   "akinsho/git-conflict.nvim",
   version = "*",
-  config = true,
-
   config = function()
-    require("git-conflict").setup{
-      default_mappings = true, -- disable buffer local mapping created by this plugin
-      default_commands = true, -- disable commands created by this plugin
-      disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
-      list_opener = 'copen', -- command or function to open the conflicts list
-      highlights = { -- They must have background color, otherwise the default color will be used
-        incoming = 'DiffAdd',
-        current = 'DiffText',
+    require("git-conflict").setup({
+      default_mappings = true, -- Keep default mappings: co (ours), ct (theirs), cb (both), c0 (none)
+      default_commands = true, -- Keep default commands
+      disable_diagnostics = false, -- Keep diagnostics visible during conflicts
+      list_opener = 'copen', -- Use quickfix list to show all conflicts
+      
+      -- Enhanced highlights for better visual distinction
+      highlights = {
+        incoming = 'DiffAdd',    -- Highlight incoming changes (theirs)
+        current = 'DiffText',    -- Highlight current changes (ours)
+      },
+      
+      -- Additional useful options
+      default_mappings_prefix = 'c', -- Prefix for default mappings (co, ct, cb, c0)
+      
+      -- Auto-detect conflict markers and set up buffer
+      auto_setup_attachment = true,
+      
+      -- Show conflict info in status line (if you use a status line plugin)
+      signs = {
+        priority = 100,
       }
-    }
+    })
+
+    -- Custom keymaps for enhanced workflow
+    local keymap = vim.keymap.set
+    keymap('n', '<leader>gl', '<cmd>GitConflictListQf<cr>', { desc = 'List git conflicts in quickfix' })
+    keymap('n', '[x', '<cmd>GitConflictPrevConflict<cr>', { desc = 'Previous conflict' })
+    keymap('n', ']x', '<cmd>GitConflictNextConflict<cr>', { desc = 'Next conflict' })
+    
+    -- Additional quick resolution mappings (optional)
+    keymap('n', '<leader>co', '<cmd>GitConflictChooseOurs<cr>', { desc = 'Choose ours (current)' })
+    keymap('n', '<leader>ct', '<cmd>GitConflictChooseTheirs<cr>', { desc = 'Choose theirs (incoming)' })
+    keymap('n', '<leader>cb', '<cmd>GitConflictChooseBoth<cr>', { desc = 'Choose both' })
+    keymap('n', '<leader>cn', '<cmd>GitConflictChooseNone<cr>', { desc = 'Choose none' })
   end
 }
