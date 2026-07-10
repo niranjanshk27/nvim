5af8a6f43b70faa02cc589a2d506c1e56b34f37b
niranjanshk <niranjanshk27@gmail.com>
2026-07-10 17:08:14 +0545

    feat: replace nvim-cmp with blink.cmp and update lazy load configurations for LSP and Treesitter

--- lua/niranjan/lazy/blink.lua
+++ lua/niranjan/lazy/blink.lua
@@ -13,7 +13,18 @@ return {
     -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     -- See the full "keymap" documentation for information on defining your own keymap.
-    keymap = { preset = 'super-tab' },
+    keymap = {
+      preset = 'default',
+      ['<M-Space>'] = { 'show', 'fallback' },
+      ['<CR>'] = { 'accept', 'fallback' },
+      ['ESC'] = { 'cancel', 'fallback' },
+      ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
+      ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
+      ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+      ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+      ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+      ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+    },
 
     appearance = {
       -- Sets the fallback highlight groups to nvim-cmp's highlight groups
@@ -22,6 +33,11 @@ return {
       -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
       -- Adjusts spacing to ensure icons are aligned
       nerd_font_variant = 'mono'
+    },
+
+    completion = {
+      documentation = { auto_show = true, auto_show_delay_ms = 500, window = { border = 'single' } },
+      menu = { border = 'single' }
     },
 
     -- Default list of enabled providers defined so that you can extend it
